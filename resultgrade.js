4f33aa9f81c3c68479a6a28b3b9ec8979b36426b
Mohammad Mansur Ali <80177222+mohammedmansurali92@users.noreply.github.com>
2023-07-27 16:56:48 +0300

    Update resultgrade.js
    More Result Marks Grade Including code

--- resultgrade.js
+++ resultgrade.js
@@ -20,4 +20,30 @@
   }
    else{
     document.write("Failed");
+  }; More result grades below:
+   var marks= prompt("Enter your Marks : ");
+
+  if(marks>100 || marks< 0){
+    document.write("Invalid");
+  }
+  else if(marks>=80 && marks<=100){
+    document.write("A+");
+  }
+  else if(marks>=70 && marks<=79){
+    document.write("A");
+  }
+  else if(marks>=60 && marks<=69){
+    document.write("A-");
+  }
+  else if(marks>=50 && marks<=59){
+    document.write("B");
+  }
+  else if(marks>=40 && marks<=49){
+    document.write("C");
+  }
+  else if(marks>=33 && marks<=39){
+    document.write("D");
+  }
+   else{
+    document.write("Failed");
   };
